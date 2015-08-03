7bf49ed59b1fdb0acec29760ceb8df297d94f851
yariplus <tafike@gmail.com>
2015-08-02 21:25:49 -0400

    Update to 0.7.x
    Fix a bunch of bugs
    Clean up admin page and add usage info
    Add html compatibility.

--- library.js
+++ library.js
@@ -1,39 +1,20 @@
-(function() {
-	"use strict";
+"use strict";
+
+(function(){
 
 	// Smile! With the power of smiles, the world becomes connected.
 
-	var Rainbow = require('./lib/rainbowvis.js'),
-		async = require('async'),
-		fs = require('fs'),
-		path = require('path'),
-		meta = module.parent.require('./meta'),
-		Settings = module.parent.require('./settings'),
-		user = module.parent.require('./user'),
-		plugins = module.parent.require('./plugins'),
+	var app, router, middleware,
+
+		Rainbows    = { },
+		Rainbow     = require('./lib/rainbowvis.js'),
+		async       = require('async'),
+		meta        = module.parent.require('./meta'),
+		Settings    = module.parent.require('./settings'),
 		SocketAdmin = module.parent.require('./socket.io/admin'),
-		translator = module.parent.require('../public/src/translator'),
-		app,
-		themes = {},
-		Rainbows = {};
+		themes = { },
 
-	Rainbows.onLoad = function (params, callback, controllers, legacyback) {
-		var router = params.router || params;
-		var middleware = params.middleware || callback;
-		app = params.app || params;
-		callback = legacyback || callback;
-
-		function render(req, res, next) {
-			res.render('admin/plugins/rainbows', {});
-		}
-
-		router.get('/admin/plugins/rainbows', middleware.admin.buildHeader, render);
-		router.get('/api/admin/plugins/rainbows', render);
-		router.get('/rainbows/config', function (req, res) {
-			res.status(200);
-		});
-
-		var	defaultSettings = {
+		defaultSettings = {
 			'themes': [
 				'flutter=magenta,yellow,range:0',
 				'dashie=red,orange,yellow,green,blue,purple,range:0',
@@ -41,15 +22,17 @@
 			]
 		};
 
-		Rainbows.settings = new Settings('rainbows', '0.0.1', defaultSettings, function() {
-			Rainbows.settings.reset(function(){
-				console.log(Rainbows.settings.get());
-				
-				loadSettings();
-			});
+	Rainbows.onLoad = function (params, callback) {
+		app        = params.app;
+		router     = params.router;
+		middleware = params.middleware;
 
-			loadSettings();
-		});
+		function renderAdmin(req, res, next) {
+			res.render('admin/plugins/rainbows', {});
+		}
+
+		router.get('/admin/plugins/rainbows', middleware.admin.buildHeader, renderAdmin);
+		router.get('/api/admin/plugins/rainbows', renderAdmin);
 
 		SocketAdmin.settings.syncRainbows = function () {
 			Rainbows.settings.sync(function(){
@@ -58,29 +41,9 @@
 		};
 
 		callback();
-	}
-	
-	function loadSettings() {
-		var config = Rainbows.settings.get();
-		themes = {};
-		if (config && config.themes && Array.isArray(config.themes)) {
-			config.themes.forEach(function (value) {
-				value = value.split('=');
-				if (value.length !== 2 || value[0] === '' || value[1] === '') return;
-				themes[value[0]] = value[1];
-			});
-		}
-	}
+	};
 
-	Rainbows.adminHeader = function (custom_header, callback) {
-		custom_header.plugins.push({
-			"route": '/plugins/rainbows',
-			"icon": 'fa-edit',
-			"name": '<b style="color:#ff0000">R</b><b style="color:#ff6000">a</b><b style="color:#ffbf00">i</b><b style="color:#dfff00">n</b><b style="color:#7fff00">b</b><b style="color:#20ff00">o</b><b style="color:#00bf40">w</b><b style="color:#00609f">s</b>'
-		});
-		
-		callback(null, custom_header);
-	}
+	Rainbows.settings = new Settings('rainbows', '0.0.1', defaultSettings, loadSettings);
 
 	var readOption = function (options, option) {
 		option = option.split(':');
@@ -113,20 +76,49 @@
 				options.colors.push(option[0]);
 			}
 		}
+	};
+
+	function loadSettings() {
+		var config = Rainbows.settings.get();
+		console.log(JSON.stringify(config));
+		themes = {};
+		if (config && config.themes && Array.isArray(config.themes)) {
+			config.themes.forEach(function (value) {
+				value = value.split('=');
+				if (value.length !== 2 || value[0] === '' || value[1] === '') return;
+				themes[value[0]] = value[1];
+			});
+		}
+	}
+
+	Rainbows.adminHeader = function (custom_header, callback) {
+		custom_header.plugins.push({
+			"route": '/plugins/rainbows',
+			"icon": '',
+			"name": '<div class="rb-nav"><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAj5JREFUeNqkVEtrE1EYPZM7j7ya2KaJSW3AWgxiMUWCgojaaLNy4cKVIP0D1a1QUNSfYf+AuBBx3YURdGOTjRu1SogNmATa0OadzON6JtRFi7aKA2eymHse3+NGkVLifx5FZjL/ct4gNMJ1HaJQMNW/IHmIyT04UNFGAh0ch+l+PEpggjg/cp7GLq6giXMQiEBx0x8lME9cQhgD3CL5IvodTWjrCKlrGPeU4FWeHyJwnVjEWUZdwk5nXAxfItp7gZhehyagTwj4p/+YYIG4iwvo8+0peX3WI8yYX5RAH+G5LmI5HfqsQFv9rcAp4h6dBcmdojeEFczYDT1qI3lnADHfRRltveqo0VpLwdLYPgF3PCuseYqx+3QWJCsNX5Ky921UIwP/Z6uXLtZaqa/bmrdnebCU2Sdwg7jKhg1Ys8bYKp1VkgXKEZz40LIX8uX+se6weTIA43SCKQ+UsMxR+dltjQ0zWLPB2DqddZLF4loJcWmZ2Sm0J3U0Do4xTVzmnB2OSme3LYTmDNZsMLZBZ51k7WYC8Ats8ewmsfNry9wnRymDS+LlnH0cVQDxXBDfEU4X6xHGjmdjSJIc4tnXbyvF2tP3q6NLpGY3NvAmlbrG9dS4YZJLIjlntnTW0X84Dhtms+YAYwdJfPjk3Wo9v1kcuT7GM6j5VitSMc0zSlAKpwZ8dCwM/AZk55sMFZrYqpXkMCLletWs3H714NN2b9dd7+7oMvFuuI3wE2OEOGStHcIizD2i+2vzr0D+FGAARenLmDXrT3oAAAAASUVORK5CYII="><b style="color:#ff0000">R</b><b style="color:#ff6000">a</b><b style="color:#ffbf00">i</b><b style="color:#dfff00">n</b><b style="color:#7fff00">b</b><b style="color:#20ff00">o</b><b style="color:#00bf40">w</b><b style="color:#00609f">s</b></div>'
+		});
+		// Image is rainbow-icon.png from:
+		// http://p.yusukekamiyamane.com/
+		// CC-BY
+		
+		callback(null, custom_header);
 	}
 
 	Rainbows.parsePost = function (data, callback) {
-		//console.log("POST: " + data.postData.content);
+
 		if (data && data.postData && data.postData.content) {
+
 			var pattern = /-=[^\0]+?=-/g;
 
 			var matches = data.postData.content.match(pattern) || [];
+
 			if (matches.length === 0) return callback(null, data);
 
 			async.each(matches, function (match, callback) {
-				//console.log("IN: " + match); //.replace(/<.*?>/g,'')
+
 				var sliced = match.slice(2, match.length-2),
-					trimmed,
+					trimmed = sliced,
+					characters,
 					rainbow = new Rainbow(),
 					html = {},
 					options = {
@@ -136,11 +128,14 @@
 						mirror: false
 					};
 
-				var postOptions = sliced.match(/\(.*?\)/);
+				var postOptions = sliced.match(/^\([^\)]*\)/);
+
 				if (postOptions) {
 					sliced = sliced.replace(postOptions[0], '');
+					trimmed = sliced;
 
 					postOptions = postOptions[0].slice(1, postOptions[0].length-1).replace(/ */g, '').split(',');
+
 					postOptions.forEach(function (option) {
 						if (option !== '') {
 							readOption(options, option);
@@ -148,23 +143,26 @@
 					});
 				}
 
-				trimmed = sliced.replace(/(\r\n|\n|\r| |\t)*/gm,'');
-				if (trimmed === '') return callback();
-				if (options.colors.length > 1) {
+				characters = sliced.replace(/<[^>]*>/gm, '').replace(/(\r\n|\n|\r| |\t)*/gm,'').length;
+
+				if (!characters) return callback(null, data);
+
+				if (options.colors.length > 0) {
+					if (options.colors.length === 1) options.colors[1] = options.colors[0];
 					try {
 						rainbow.setSpectrumByArray(options.colors);
 					} catch (e) {
 						data.postData.content = data.postData.content.replace(match, sliced);
-						return callback();
+						return callback(null, data);
 					}
 					if (options.range === 0) {
-						options.range = trimmed.length > 1 ? trimmed.length : 2;
+						options.range = characters > 1 ? characters : 2;
 					}else{
 						options.range = options.range > options.colors.length ? options.range : options.colors.length;
 					}
 				}else{
 					if (options.range === 0) {
-						options.range = trimmed.length > 8 ? trimmed.length : 8;
+						options.range = characters > 8 ? characters : 8;
 					}else{
 						options.range = options.range > 8 ? options.range : 8;
 					}
@@ -173,22 +171,32 @@
 
 				var parsed = "",
 					offset = 0;
-				for (var i = 0; i < sliced.length; i++) {
-					var c = sliced.charAt(i);
-					if (c.match(/\n/)) {
+
+				while (trimmed.length) {
+					var c = trimmed.charAt(0);
+
+					if (c.match(/(\n|\r)/)) {
 						parsed += '<br>';
-						offset++;
+						trimmed = trimmed.substr(1);
 					}else if (c.match(/ /)) {
 						parsed += ' ';
-						offset++;
+						trimmed = trimmed.substr(1);
+					}else if (trimmed.match(/^<\/p><p>/)) {
+						parsed += '<br /><br />';
+						trimmed = trimmed.replace(/^<\/p><p>/, '');
+					}else if (trimmed.match(/^<[^>]*>/)) {
+						parsed += trimmed.match(/^<[^>]*>/)[0];
+						trimmed = trimmed.replace(/^<[^>]*>/, '');
 					}else{
-						parsed += '<span style="color:#' + rainbow.colourAt((i - offset) % options.range) + '">' + sliced.charAt(i) + '</span>';
+						parsed += '<span style="color:#' + rainbow.colourAt(offset % options.range) + '">' + trimmed.charAt(0) + '</span>';
+						trimmed = trimmed.substr(1);
+						offset++;
 					}
 				}
 				if (options.bgcolor) parsed = '<span style="background-color:' + options.bgcolor + '">' + parsed + '</span>';
 
 				data.postData.content = data.postData.content.replace(match, parsed);
-				//console.log("OUT: " + parsed);
+
 				callback();
 			}, function(err){
 				callback(null, data);
@@ -198,11 +206,6 @@
 		}
 	}
 
-	Rainbows.parseRaw = function (data, callback) {
-		Rainbows.parsePost({postData: {content: data}, uid: -1}, function(err, data) {
-			callback(err, data.postData.content);
-		});
-	}
-
 	module.exports = Rainbows;
-})();
+
+}(module));
--- public/templates/admin/plugins/rainbows.tpl
+++ public/templates/admin/plugins/rainbows.tpl
@@ -4,42 +4,29 @@
             <div class="panel acp-panel-primary">
                 <div class="panel-heading">
                     <div class="panel-title">
-                        <span style="color:#ff0000">R</span><span style="color:#ff6000">a</span><span style="color:#ffbf00">i</span><span style="color:#dfff00">n</span><span style="color:#7fff00">b</span><span style="color:#20ff00">o</span><span style="color:#00bf40">w</span><span style="color:#00609f">s</span> <span style="color:#00609f">S</span><span style="color:#ff0000">e</span><span style="color:#ff6000">t</span><span style="color:#ffbf00">t</span><span style="color:#dfff00">i</span><span style="color:#7fff00">n</span><span style="color:#20ff00">g</span><span style="color:#00bf40">s</span>
+                        <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAj5JREFUeNqkVEtrE1EYPZM7j7ya2KaJSW3AWgxiMUWCgojaaLNy4cKVIP0D1a1QUNSfYf+AuBBx3YURdGOTjRu1SogNmATa0OadzON6JtRFi7aKA2eymHse3+NGkVLifx5FZjL/ct4gNMJ1HaJQMNW/IHmIyT04UNFGAh0ch+l+PEpggjg/cp7GLq6giXMQiEBx0x8lME9cQhgD3CL5IvodTWjrCKlrGPeU4FWeHyJwnVjEWUZdwk5nXAxfItp7gZhehyagTwj4p/+YYIG4iwvo8+0peX3WI8yYX5RAH+G5LmI5HfqsQFv9rcAp4h6dBcmdojeEFczYDT1qI3lnADHfRRltveqo0VpLwdLYPgF3PCuseYqx+3QWJCsNX5Ky921UIwP/Z6uXLtZaqa/bmrdnebCU2Sdwg7jKhg1Ys8bYKp1VkgXKEZz40LIX8uX+se6weTIA43SCKQ+UsMxR+dltjQ0zWLPB2DqddZLF4loJcWmZ2Sm0J3U0Do4xTVzmnB2OSme3LYTmDNZsMLZBZ51k7WYC8Ats8ewmsfNry9wnRymDS+LlnH0cVQDxXBDfEU4X6xHGjmdjSJIc4tnXbyvF2tP3q6NLpGY3NvAmlbrG9dS4YZJLIjlntnTW0X84Dhtms+YAYwdJfPjk3Wo9v1kcuT7GM6j5VitSMc0zSlAKpwZ8dCwM/AZk55sMFZrYqpXkMCLletWs3H714NN2b9dd7+7oMvFuuI3wE2OEOGStHcIizD2i+2vzr0D+FGAARenLmDXrT3oAAAAASUVORK5CYII=" width="50px" height="50px"> 
+						<b style="color:#ff0000">R</b><b style="color:#ff6000">a</b><b style="color:#ffbf00">i</b><b style="color:#dfff00">n</b><b style="color:#7fff00">b</b><b style="color:#20ff00">o</b><b style="color:#00bf40">w</b><b style="color:#00609f">s</b>
                     </div>
                 </div>
                 <div class="panel-body">
+				    <h3>Usage</h3>
+					<p>
+						Surround text with "-= =-"" like so: -=Rainbow Text=-<br>
+						Add options in parathesis at the start of the rainbow text like so: -=(red,blue,green)Rainbow Text=-<br>
+						Options include any colors, "range:number", "theme:name", and "bg:color".
+					</p>
                     <h3>Themes</h3>
                     <p>
                         Format for themes is "name=color,color,color"
                         
-                        You can use as many colors as you can imagine. Invalid colors will render text black. Valid options are "repeat:number" and "repeat:every".
+                        You can use as many colors as you can imagine. Invalid colors will render text black. You can also use "range:X" in place of a color to tell the plugin to repeat the pattern after X number of characters.
                     </p>
                     <div class="form-group">
                         <div data-key="themes" data-attributes='{"data-type":"input", "style":"width:80%;margin-bottom:10px;"}' data-split="<br>" data-new='' style="width:100%;"></div>
                     </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-3">
-            <div class="panel acp-panel-primary">
-                <div class="panel-heading">
-                    Action Panel
-                </div>
-
-                <div class="panel-body">
-                    <div class="form-group">
-                        <button type="button" class="btn btn-warning form-control" id="reset">
-                            <i class="fa fa-fw fa-history"></i> Reset Settings
-                        </button>
-                    </div>
-                    <div class="form-group">
-                        <button type="button" class="btn btn-danger form-control" id="clear">
-                            <i class="fa fa-fw fa-times"></i> Clear Settings
-                        </button>
-                    </div>
-                    <div class="form-group">
+					<div class="form-group">
                         <button type="button" class="btn btn-success form-control" id="save">
-                            <i class="fa fa-fw fa-save"></i> Save Settings
+                            <i class="fa fa-fw fa-save"></i> Save Themes
                         </button>
                     </div>
                 </div>
