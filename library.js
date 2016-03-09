bb1428557798eacd0d83f6eea5932b535929f251
yariplus <tafike@gmail.com>
2016-03-09 03:21:06 -0500

    Change settings page.

--- library.js
+++ library.js
@@ -1,268 +1,265 @@
-"use strict";
+// Smile! With the power of smiles, the world becomes connected.
 
-(function(Rainbows, NodeBB){
+"use strict"
 
-	// Smile! With the power of smiles, the world becomes connected.
+var plugin = module.exports
 
-	var app, router, middleware,
+var RainbowVis    = require('./lib/rainbowvis.js')
+var async         = require('async')
 
-		RainbowVis    = require('./lib/rainbowvis.js'),
-		async         = require('async'),
+var NodeBB        = module.parent
+var meta          = NodeBB.require('./meta')
+var Settings      = NodeBB.require('./settings')
+var SocketAdmin   = NodeBB.require('./socket.io/admin')
+var SocketPlugins = NodeBB.require('./socket.io/plugins')
 
-		meta          = NodeBB.require('./meta'),
-		Settings      = NodeBB.require('./settings'),
-		SocketAdmin   = NodeBB.require('./socket.io/admin'),
-		SocketPlugins = NodeBB.require('./socket.io/plugins'),
+var themes = { }
 
-		themes = { },
+var defaultSettings = {
+	themes: [
+		{ name: 'flutter', value: 'magenta,yellow' },
+		{ name: 'dashie',  value: 'red,orange,yellow,green,blue,purple' },
+		{ name: 'sunbutt', value: 'lightblue,lightpink,lightgreen' }
+	],
+	postsEnabled   : 1,
+	tagsEnabled    : 1,
+	topicsEnabled  : 1,
+	postsModsOnly  : 0,
+	topicsModsOnly : 0,
+	hueModifier : 0,
+	lumModifier : 40
+}
 
-		defaultSettings = {
-			themes: [
-				'flutter=magenta,yellow',
-				'dashie=red,orange,yellow,green,blue,purple',
-				'sunbutt=lightblue,lightpink,lightgreen'
-			],
-			rainbowifyTags: 1,
-			hueModifier: 0,
-			lumModifier: 40
-		};
+plugin.onLoad = function (params, callback) {
+	var router     = params.router;
+	var middleware = params.middleware;
 
-	Rainbows.onLoad = function (params, callback) {
-		app        = params.app;
-		router     = params.router;
-		middleware = params.middleware;
+	plugin.settings = new Settings('rainbows', '1.2.0', defaultSettings, loadSettings);
 
-		function renderAdmin(req, res, next) {
-			res.render('admin/plugins/rainbows', {});
-		}
+	function renderAdmin(req, res, next) {
+		res.render('admin/plugins/rainbows', {});
+	}
 
-		router.get('/admin/plugins/rainbows', middleware.admin.buildHeader, renderAdmin);
-		router.get('/api/admin/plugins/rainbows', renderAdmin);
+	router.get('/admin/plugins/rainbows', middleware.admin.buildHeader, renderAdmin);
+	router.get('/api/admin/plugins/rainbows', renderAdmin);
 
-		SocketAdmin.settings.syncRainbows = function () {
-			Rainbows.settings.sync(loadSettings);
-		};
-
-		SocketPlugins.rainbows = {
-			rainbowify: function (socket, data, next) {
-				Rainbows.parseRaw(data.text, next);
-			}
-		};
-		callback();
+	SocketAdmin.settings.syncRainbows = function () {
+		plugin.settings.sync(loadSettings);
 	};
 
-	Rainbows.settings = new Settings('rainbows', '1.2.0', defaultSettings, loadSettings);
-
-	var readOption = function (options, option) {
-		option = option.split(':');
-		if (option.length > 1 && option[0] !== '' && option[1] !== '') {
-			switch (option[0]) {
-				case 'range':
-					options.range = Math.floor(parseInt(option[1]));
-					options.range = options.range > -1 && options.range < 9000 ? options.range : 0;
-					break;
-				case 'theme':
-					if (themes.hasOwnProperty(option[1]) && !!themes[option[1]]) {
-						themes[option[1]].replace(/ */g, '').split(',').forEach(function(val){
-							if (val !== '') {
-								readOption(options, val);
-							}
-						});
-					}
-					break;
-				case 'bg':
-				case 'bgcolor':
-					options.bgcolor = option[1];
-					break;
-				default:
-					break;
-			}
-		}else{
-			if (option[0] === 'mirror') {
-				options.mirror = true;
-			}else{
-				options.colors.push(option[0]);
-			}
+	SocketPlugins.rainbows = {
+		rainbowify: function (socket, data, next) {
+			plugin.parseRaw(data.text, next);
 		}
 	};
 
 	function loadSettings() {
-		var config = Rainbows.settings.get();
-
-		themes = {};
-		if (config && config.themes && Array.isArray(config.themes)) {
-			config.themes.forEach(function (value) {
-				value = value.split('=');
-				if (value.length !== 2 || value[0] === '' || value[1] === '') return;
-				themes[value[0]] = value[1];
-			});
-		}
-	}
-
-	Rainbows.adminHeader = function (custom_header, callback) {
-		custom_header.plugins.push({
-			"route": '/plugins/rainbows',
-			"icon": '',
-			"name": '<div class="rb-nav"><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAj5JREFUeNqkVEtrE1EYPZM7j7ya2KaJSW3AWgxiMUWCgojaaLNy4cKVIP0D1a1QUNSfYf+AuBBx3YURdGOTjRu1SogNmATa0OadzON6JtRFi7aKA2eymHse3+NGkVLifx5FZjL/ct4gNMJ1HaJQMNW/IHmIyT04UNFGAh0ch+l+PEpggjg/cp7GLq6giXMQiEBx0x8lME9cQhgD3CL5IvodTWjrCKlrGPeU4FWeHyJwnVjEWUZdwk5nXAxfItp7gZhehyagTwj4p/+YYIG4iwvo8+0peX3WI8yYX5RAH+G5LmI5HfqsQFv9rcAp4h6dBcmdojeEFczYDT1qI3lnADHfRRltveqo0VpLwdLYPgF3PCuseYqx+3QWJCsNX5Ky921UIwP/Z6uXLtZaqa/bmrdnebCU2Sdwg7jKhg1Ys8bYKp1VkgXKEZz40LIX8uX+se6weTIA43SCKQ+UsMxR+dltjQ0zWLPB2DqddZLF4loJcWmZ2Sm0J3U0Do4xTVzmnB2OSme3LYTmDNZsMLZBZ51k7WYC8Ats8ewmsfNry9wnRymDS+LlnH0cVQDxXBDfEU4X6xHGjmdjSJIc4tnXbyvF2tP3q6NLpGY3NvAmlbrG9dS4YZJLIjlntnTW0X84Dhtms+YAYwdJfPjk3Wo9v1kcuT7GM6j5VitSMc0zSlAKpwZ8dCwM/AZk55sMFZrYqpXkMCLletWs3H714NN2b9dd7+7oMvFuuI3wE2OEOGStHcIizD2i+2vzr0D+FGAARenLmDXrT3oAAAAASUVORK5CYII="><b style="color:#ff0000">R</b><b style="color:#ff6000">a</b><b style="color:#ffbf00">i</b><b style="color:#dfff00">n</b><b style="color:#7fff00">b</b><b style="color:#20ff00">o</b><b style="color:#00bf40">w</b><b style="color:#00609f">s</b></div>'
+		var themes = {};
+		plugin.settings.get('themes').forEach(function(theme){
+			themes[theme.name] = theme.value;
 		});
-		// Image is rainbow-icon.png from:
-		// http://p.yusukekamiyamane.com/
-		// CC-BY
-
-		callback(null, custom_header);
+		plugin.settings.set('themes', themes);
+		console.dir(plugin.settings.get());
 	}
 
-	Rainbows.parseRaw = function (data, callback) {
-		if (data) {
+	callback();
+}
 
-			var pattern = /-=[^\0]+?=-/g;
-
-			var matches = data.match(pattern) || [];
-
-			if (matches.length === 0) return callback(null, data);
-
-			async.each(matches, function (match, callback) {
-
-				var sliced = match.slice(2, match.length-2),
-					trimmed = sliced,
-					characters,
-					rainbow = new RainbowVis(),
-					html = {},
-					options = {
-						range: 0,
-						colors: [],
-						bgcolor: '',
-						mirror: false
-					};
-
-				var postOptions = sliced.match(/^\([^\)]*\)/);
-
-				if (postOptions) {
-					sliced = sliced.replace(postOptions[0], '');
-					trimmed = sliced;
-
-					postOptions = postOptions[0].slice(1, postOptions[0].length-1).replace(/ */g, '').split(',');
-
-					postOptions.forEach(function (option) {
-						if (option !== '') {
-							readOption(options, option);
+var readOption = function (options, option) {
+	option = option.split(':');
+	if (option.length > 1 && option[0] !== '' && option[1] !== '') {
+		switch (option[0]) {
+			case 'range':
+				options.range = Math.floor(parseInt(option[1]));
+				options.range = options.range > -1 && options.range < 9000 ? options.range : 0;
+				break;
+			case 'theme':
+				if (themes.hasOwnProperty(option[1]) && !!themes[option[1]]) {
+					themes[option[1]].replace(/ */g, '').split(',').forEach(function(val){
+						if (val !== '') {
+							readOption(options, val);
 						}
 					});
 				}
-
-				characters = sliced.replace(/<[^>]*>/gm, '').replace(/(\r\n|\n|\r| |\t)*/gm,'').length;
-
-				if (!characters) return callback(null, data);
-
-				if (options.colors.length > 0) {
-					if (options.colors.length === 1) options.colors[1] = options.colors[0];
-
-					if (options.range === 0) {
-						options.range = characters > 1 ? characters : 2;
-					}else{
-						options.range = options.range > options.colors.length ? options.range : options.colors.length;
-					}
-				}else{
-					options.colors = [ 'red', 'orange', 'gold', 'lime', 'deepskyblue', 'blue', 'blueviolet', 'magenta'];
-
-					if (options.range === 0) {
-						options.range = characters > 8 ? characters : 8;
-					}else{
-						options.range = options.range > 8 ? options.range : 8;
-					}
-				}
-
-				try {
-					rainbow.setSpectrumByArray(options.colors);
-				} catch (e) {
-					data = data.replace(match, sliced);
-					return callback(null, data);
-				}
-
-				rainbow.setNumberRange(0, options.range - 1);
-
-				var parsed = "",
-					offset = 0;
-
-				while (trimmed.length) {
-					var c = trimmed.charAt(0);
-
-					if (c.match(/(\n|\r)/)) {
-						parsed += '<br>';
-						trimmed = trimmed.substr(1);
-					}else if (c.match(/ /)) {
-						parsed += ' ';
-						trimmed = trimmed.substr(1);
-					}else if (trimmed.match(/^<\/p><p>/)) {
-						parsed += '<br /><br />';
-						trimmed = trimmed.replace(/^<\/p><p>/, '');
-					}else if (trimmed.match(/^<[^>]*>/)) {
-						parsed += trimmed.match(/^<[^>]*>/)[0];
-						trimmed = trimmed.replace(/^<[^>]*>/, '');
-					}else{
-						parsed += '<span style="color:#' + rainbow.colourAt(offset % options.range) + '">' + trimmed.charAt(0) + '</span>';
-						trimmed = trimmed.substr(1);
-						offset++;
-					}
-				}
-
-				if (options.bgcolor) parsed = '<span style="background-color:' + options.bgcolor + '">' + parsed + '</span>';
-
-				parsed = '<span class="rainbowified">' + parsed + '</span>';
-
-				data = data.replace(match, parsed);
-
-				callback();
-			}, function(err){
-				callback(null, data);
-			});
-		}else{
-			callback(null, data);
+				break;
+			case 'bg':
+			case 'bgcolor':
+				options.bgcolor = option[1];
+				break;
+			default:
+				break;
 		}
-	};
+	}else{
+		if (option[0] === 'mirror') {
+			options.mirror = true;
+		}else{
+			options.colors.push(option[0]);
+		}
+	}
+};
 
-	Rainbows.parseSignature = function (data, callback) {
-		Rainbows.parseRaw(data.userData.signature, function (err, signature) {
-			data.userData.signature = signature;
-			callback(err, data);
-		});
-	};
+plugin.adminHeader = function (custom_header, callback) {
+	custom_header.plugins.push({
+		"route": '/plugins/rainbows',
+		"icon": '',
+		"name": '<div class="rb-nav"><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAj5JREFUeNqkVEtrE1EYPZM7j7ya2KaJSW3AWgxiMUWCgojaaLNy4cKVIP0D1a1QUNSfYf+AuBBx3YURdGOTjRu1SogNmATa0OadzON6JtRFi7aKA2eymHse3+NGkVLifx5FZjL/ct4gNMJ1HaJQMNW/IHmIyT04UNFGAh0ch+l+PEpggjg/cp7GLq6giXMQiEBx0x8lME9cQhgD3CL5IvodTWjrCKlrGPeU4FWeHyJwnVjEWUZdwk5nXAxfItp7gZhehyagTwj4p/+YYIG4iwvo8+0peX3WI8yYX5RAH+G5LmI5HfqsQFv9rcAp4h6dBcmdojeEFczYDT1qI3lnADHfRRltveqo0VpLwdLYPgF3PCuseYqx+3QWJCsNX5Ky921UIwP/Z6uXLtZaqa/bmrdnebCU2Sdwg7jKhg1Ys8bYKp1VkgXKEZz40LIX8uX+se6weTIA43SCKQ+UsMxR+dltjQ0zWLPB2DqddZLF4loJcWmZ2Sm0J3U0Do4xTVzmnB2OSme3LYTmDNZsMLZBZ51k7WYC8Ats8ewmsfNry9wnRymDS+LlnH0cVQDxXBDfEU4X6xHGjmdjSJIc4tnXbyvF2tP3q6NLpGY3NvAmlbrG9dS4YZJLIjlntnTW0X84Dhtms+YAYwdJfPjk3Wo9v1kcuT7GM6j5VitSMc0zSlAKpwZ8dCwM/AZk55sMFZrYqpXkMCLletWs3H714NN2b9dd7+7oMvFuuI3wE2OEOGStHcIizD2i+2vzr0D+FGAARenLmDXrT3oAAAAASUVORK5CYII="><b style="color:#ff0000">R</b><b style="color:#ff6000">a</b><b style="color:#ffbf00">i</b><b style="color:#dfff00">n</b><b style="color:#7fff00">b</b><b style="color:#20ff00">o</b><b style="color:#00bf40">w</b><b style="color:#00609f">s</b></div>'
+	});
+	// Image is rainbow-icon.png from:
+	// http://p.yusukekamiyamane.com/
+	// CC-BY
 
-	Rainbows.parsePost = function (data, callback) {
-		Rainbows.parseRaw(data.postData.content, function (err, content) {
-			data.postData.content = content;
-			callback(err, data);
-		});
-	};
+	callback(null, custom_header);
+}
 
-	Rainbows.parseTopic = function (data, callback) {
-		Rainbows.parseRaw(data.topic.title, function (err, title) {
-			data.topic.title = title;
-			console.dir(title);
-			callback(err, data);
-		});
-	};
+plugin.parseRaw = function (data, callback) {
+	if (data) {
 
-	Rainbows.parseTopics = function (data, callback) {
-		var topics = [];
-		async.eachSeries(data.topics, function (topic, next) {
-			Rainbows.parseRaw(topic.title, function (err, title) {
-				topic.title = title;
-				topics.push(topic);
-				next();
-			});
-		}, function () {
-			data.topics = topics;
+		var pattern = /-=[^\0]+?=-/g;
+
+		var matches = data.match(pattern) || [];
+
+		if (matches.length === 0) return callback(null, data);
+
+		async.each(matches, function (match, callback) {
+
+			var sliced = match.slice(2, match.length-2),
+				trimmed = sliced,
+				characters,
+				rainbow = new RainbowVis(),
+				html = {},
+				options = {
+					range: 0,
+					colors: [],
+					bgcolor: '',
+					mirror: false
+				};
+
+			var postOptions = sliced.match(/^\([^\)]*\)/);
+
+			if (postOptions) {
+				sliced = sliced.replace(postOptions[0], '');
+				trimmed = sliced;
+
+				postOptions = postOptions[0].slice(1, postOptions[0].length-1).replace(/ */g, '').split(',');
+
+				postOptions.forEach(function (option) {
+					if (option !== '') {
+						readOption(options, option);
+					}
+				});
+			}
+
+			characters = sliced.replace(/<[^>]*>/gm, '').replace(/(\r\n|\n|\r| |\t)*/gm,'').length;
+
+			if (!characters) return callback(null, data);
+
+			if (options.colors.length > 0) {
+				if (options.colors.length === 1) options.colors[1] = options.colors[0];
+
+				if (options.range === 0) {
+					options.range = characters > 1 ? characters : 2;
+				}else{
+					options.range = options.range > options.colors.length ? options.range : options.colors.length;
+				}
+			}else{
+				options.colors = [ 'red', 'orange', 'gold', 'lime', 'deepskyblue', 'blue', 'blueviolet', 'magenta'];
+
+				if (options.range === 0) {
+					options.range = characters > 8 ? characters : 8;
+				}else{
+					options.range = options.range > 8 ? options.range : 8;
+				}
+			}
+
+			try {
+				rainbow.setSpectrumByArray(options.colors);
+			} catch (e) {
+				data = data.replace(match, sliced);
+				return callback(null, data);
+			}
+
+			rainbow.setNumberRange(0, options.range - 1);
+
+			var parsed = "",
+				offset = 0;
+
+			while (trimmed.length) {
+				var c = trimmed.charAt(0);
+
+				if (c.match(/(\n|\r)/)) {
+					parsed += '<br>';
+					trimmed = trimmed.substr(1);
+				}else if (c.match(/ /)) {
+					parsed += ' ';
+					trimmed = trimmed.substr(1);
+				}else if (trimmed.match(/^<\/p><p>/)) {
+					parsed += '<br /><br />';
+					trimmed = trimmed.replace(/^<\/p><p>/, '');
+				}else if (trimmed.match(/^<[^>]*>/)) {
+					parsed += trimmed.match(/^<[^>]*>/)[0];
+					trimmed = trimmed.replace(/^<[^>]*>/, '');
+				}else{
+					parsed += '<span style="color:#' + rainbow.colourAt(offset % options.range) + '">' + trimmed.charAt(0) + '</span>';
+					trimmed = trimmed.substr(1);
+					offset++;
+				}
+			}
+
+			if (options.bgcolor) parsed = '<span style="background-color:' + options.bgcolor + '">' + parsed + '</span>';
+
+			parsed = '<span class="rainbowified">' + parsed + '</span>';
+
+			data = data.replace(match, parsed);
+
+			callback();
+		}, function(err){
 			callback(null, data);
 		});
-	};
+	}else{
+		callback(null, data);
+	}
+};
 
-	Rainbows.configGet = function (data, next) {
-		data.rainbowifyTags = parseInt(Rainbows.settings.get('rainbowifyTags'), 10) === 1;
-		data.hueModifier = parseInt(Rainbows.settings.get('hueModifier'), 10) || 0;
-		data.lumModifier = parseInt(Rainbows.settings.get('lumModifier'), 10) || 40;
+plugin.parseSignature = function (data, callback) {
+	plugin.parseRaw(data.userData.signature, function (err, signature) {
+		data.userData.signature = signature;
+		callback(err, data);
+	});
+};
 
-		next(null, data);
-	};
+plugin.parsePost = function (data, callback) {
+	plugin.parseRaw(data.postData.content, function (err, content) {
+		data.postData.content = content;
+		callback(err, data);
+	});
+};
 
-}(module.exports, module.parent));
+plugin.parseTopic = function (data, callback) {
+	plugin.parseRaw(data.topic.title, function (err, title) {
+		data.topic.title = title;
+		console.dir(title);
+		callback(err, data);
+	});
+};
+
+plugin.parseTopics = function (data, callback) {
+	var topics = [];
+	async.eachSeries(data.topics, function (topic, next) {
+		plugin.parseRaw(topic.title, function (err, title) {
+			topic.title = title;
+			topics.push(topic);
+			next();
+		});
+	}, function () {
+		data.topics = topics;
+		callback(null, data);
+	});
+};
+
+plugin.configGet = function (data, next) {
+	data.rainbowifyTags = parseInt(plugin.settings.get('rainbowifyTags'), 10) === 1;
+	data.hueModifier = parseInt(plugin.settings.get('hueModifier'), 10) || 0;
+	data.lumModifier = parseInt(plugin.settings.get('lumModifier'), 10) || 40;
+
+	next(null, data);
+};
--- public/templates/admin/plugins/rainbows.tpl
+++ public/templates/admin/plugins/rainbows.tpl
@@ -1,82 +1,140 @@
 <form class="form" id="rainbows">
-	<div class="row">
-		<div class="col-lg-9">
-			<div class="panel acp-panel-primary">
-				<div class="panel-heading">
-					<div class="panel-title">
-						<img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAj5JREFUeNqkVEtrE1EYPZM7j7ya2KaJSW3AWgxiMUWCgojaaLNy4cKVIP0D1a1QUNSfYf+AuBBx3YURdGOTjRu1SogNmATa0OadzON6JtRFi7aKA2eymHse3+NGkVLifx5FZjL/ct4gNMJ1HaJQMNW/IHmIyT04UNFGAh0ch+l+PEpggjg/cp7GLq6giXMQiEBx0x8lME9cQhgD3CL5IvodTWjrCKlrGPeU4FWeHyJwnVjEWUZdwk5nXAxfItp7gZhehyagTwj4p/+YYIG4iwvo8+0peX3WI8yYX5RAH+G5LmI5HfqsQFv9rcAp4h6dBcmdojeEFczYDT1qI3lnADHfRRltveqo0VpLwdLYPgF3PCuseYqx+3QWJCsNX5Ky921UIwP/Z6uXLtZaqa/bmrdnebCU2Sdwg7jKhg1Ys8bYKp1VkgXKEZz40LIX8uX+se6weTIA43SCKQ+UsMxR+dltjQ0zWLPB2DqddZLF4loJcWmZ2Sm0J3U0Do4xTVzmnB2OSme3LYTmDNZsMLZBZ51k7WYC8Ats8ewmsfNry9wnRymDS+LlnH0cVQDxXBDfEU4X6xHGjmdjSJIc4tnXbyvF2tP3q6NLpGY3NvAmlbrG9dS4YZJLIjlntnTW0X84Dhtms+YAYwdJfPjk3Wo9v1kcuT7GM6j5VitSMc0zSlAKpwZ8dCwM/AZk55sMFZrYqpXkMCLletWs3H714NN2b9dd7+7oMvFuuI3wE2OEOGStHcIizD2i+2vzr0D+FGAARenLmDXrT3oAAAAASUVORK5CYII=" width="50px" height="50px"> 
-						<b style="color:#ff0000">R</b><b style="color:#ffa500">a</b><b style="color:#ffd700">i</b><b style="color:#00ff00">n</b><b style="color:#00bfff">b</b><b style="color:#0000ff">o</b><b style="color:#8a2be2">w</b><b style="color:#ff00ff">s</b>
-					</div>
-				</div>
-				<div class="panel-body">
-				    <h3>Usage</h3>
-					<p>Surround text with "-= =-" to make rainbows.</p>
-					<p><code>-=Rainbow Text=-</code></p>
-					<p>Add options with parenthesis like so:</p>
-					<p><code>-=(red,yellow,blue,range:3)Rainbow Text=-</code></p>
-					<p>Options include:</p>
+	<button type="button" class="btn btn-success form-control rainbows-save"><i class="fa fa-fw fa-save"></i> Save Settings</button>
+
+	<div class="h1 hidden">
+		<img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAj5JREFUeNqkVEtrE1EYPZM7j7ya2KaJSW3AWgxiMUWCgojaaLNy4cKVIP0D1a1QUNSfYf+AuBBx3YURdGOTjRu1SogNmATa0OadzON6JtRFi7aKA2eymHse3+NGkVLifx5FZjL/ct4gNMJ1HaJQMNW/IHmIyT04UNFGAh0ch+l+PEpggjg/cp7GLq6giXMQiEBx0x8lME9cQhgD3CL5IvodTWjrCKlrGPeU4FWeHyJwnVjEWUZdwk5nXAxfItp7gZhehyagTwj4p/+YYIG4iwvo8+0peX3WI8yYX5RAH+G5LmI5HfqsQFv9rcAp4h6dBcmdojeEFczYDT1qI3lnADHfRRltveqo0VpLwdLYPgF3PCuseYqx+3QWJCsNX5Ky921UIwP/Z6uXLtZaqa/bmrdnebCU2Sdwg7jKhg1Ys8bYKp1VkgXKEZz40LIX8uX+se6weTIA43SCKQ+UsMxR+dltjQ0zWLPB2DqddZLF4loJcWmZ2Sm0J3U0Do4xTVzmnB2OSme3LYTmDNZsMLZBZ51k7WYC8Ats8ewmsfNry9wnRymDS+LlnH0cVQDxXBDfEU4X6xHGjmdjSJIc4tnXbyvF2tP3q6NLpGY3NvAmlbrG9dS4YZJLIjlntnTW0X84Dhtms+YAYwdJfPjk3Wo9v1kcuT7GM6j5VitSMc0zSlAKpwZ8dCwM/AZk55sMFZrYqpXkMCLletWs3H714NN2b9dd7+7oMvFuuI3wE2OEOGStHcIizD2i+2vzr0D+FGAARenLmDXrT3oAAAAASUVORK5CYII=" width="50px" height="50px"> 
+		<b style="color:#ff0000">R</b><b style="color:#ffa500">a</b><b style="color:#ffd700">i</b><b style="color:#00ff00">n</b><b style="color:#00bfff">b</b><b style="color:#0000ff">o</b><b style="color:#8a2be2">w</b><b style="color:#ff00ff">s</b>
+	</div>
+
+	<div class="panel panel-success">
+		<div class="panel-heading btn btn-success pointer" data-toggle="collapse" data-target=".help">
+			<h3 class="panel-title"><i class="fa fa-caret-down pull-right"></i>Help!</h3>
+		</div>
+		<div class="panel-body collapse help">
+			<h4>Manual Usage</h4>
+			<p>Surround text with "-= =-" to make rainbows.</p>
+			<p><code>-=Rainbow Text=-</code></p>
+			<p>Add options with parenthesis like so:</p>
+			<p><code>-=(red,yellow,blue,range:3)Rainbow Text=-</code></p>
+			<p>Options include:</p>
+			<ul>
+				<li>Any CSS valid color
 					<ul>
-						<li>Any CSS valid color
-							<ul>
-								<li>Adds that color to the spectrum used in the rainbow.</li>
-							</ul>
-						</li>
-						<li>range:&#123;number&#125;
-							<ul>
-								<li>Repeats the spectrum after &#123;number&#125; characters, instead of stretching it over the whole text.</li>
-							</ul>
-						</li>
-						<li>bg:&#123;color&#125;
-							<ul>
-								<li>Puts a background color &#123;color&#125; behind the text.</li>
-							</ul>
-						</li>
-						<li>theme:&#123;name&#125;
-							<ul>
-								<li>Uses the theme &#123;name&#125; to make the rainbow. Themes are created below.</li>
-							</ul>
-						</li>
+						<li>Adds that color to the spectrum used in the rainbow.</li>
 					</ul>
-					<h3>Themes</h3>
-					<p>
-						Format for themes is "&#123;name&#125;=&#123;options&#125;". Available &#123;options&#125; are the same as above.
-					</p>
+				</li>
+				<li>range:&#123;number&#125;
+					<ul>
+						<li>Repeats the spectrum after &#123;number&#125; characters, instead of stretching it over the whole text.</li>
+					</ul>
+				</li>
+				<li>bg:&#123;color&#125;
+					<ul>
+						<li>Puts a background color &#123;color&#125; behind the text.</li>
+					</ul>
+				</li>
+				<li>theme:&#123;name&#125;
+					<ul>
+						<li>Uses the theme &#123;name&#125; to make the rainbow. Themes are created below.</li>
+					</ul>
+				</li>
+			</ul>
+
+			<div>Blueberry, blackberry, raspberry, strawberry.</div>
+
+			<div>apple banana cherry date eggfruit fig grapefruit honeydew ilama jambolan kiwi lemon mango nectarine orange pineapple quandong raspberry strawberry tomato umeboshi voavanga watermelon xigua yellow zucchini</div>
+		</div>
+	</div>
+
+	<div class="panel panel-primary">
+		<div class="panel-heading"><span class="panel-title">Posts</span></div>
+		<div class="panel-body">
+			<div class="form-group">
+				<div class="checkbox">
+					<label for="postsEnabled">
+						<input data-key="postsEnabled" id="postsEnabled" type="checkbox">
+						Enable colors in posts.
+					</label>
+				</div>
+				<div class="checkbox">
+					<label for="postsModsOnly">
+						<input data-key="postsModsOnly" id="postsModsOnly" type="checkbox">
+						Only allow moderators to use colors in posts.
+					</label>
+				</div>
+			</div>
+
+			<div class="panel panel-success">
+				<div class="panel-heading btn btn-success pointer" data-toggle="collapse" data-target=".themes">
+					<h3 class="panel-title"><i class="fa fa-caret-down pull-right"></i>Preset Themes</h3>
+				</div>
+				<div class="panel-body collapse themes">
 					<div class="form-group">
-						<div data-key="themes" data-attributes='{"data-type":"input", "style":"width:80%;margin-bottom:10px;"}' data-split="<br>" data-new='' style="width:100%;"></div>
-					</div>
-					<div class="form-group">
-						<button type="button" class="btn btn-success form-control" id="save">
-							<i class="fa fa-fw fa-save"></i> Save Themes
-						</button>
+						<span class="theme-label one">Name</span>
+						<span class="theme-label">Options</span>
+						<div data-key="themes" data-split='<br>' data-attributes='{
+							"tagName":"span",
+							"class":"theme",
+							"data-type":"object",
+							"data-split":" ",
+							"data-properties":[
+								{
+									"data-prop":"name"
+								},
+								{
+									"data-prop":"value"
+								}
+							]}'>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-	<div class="row">
-		<div class="col-lg-9">
-			<div class="panel acp-panel-primary">
-				<div class="panel-body">
-				    <h3>Tags</h3>
-					<div class="form-group">
-						<label for="rainbowifyTags">
-							<input type="checkbox" data-key="rainbowifyTags" id="rainbowifyTags" />
-							Rainbowify Tags
-						</label>
-					</div>
-					<div class="form-group">
-						<label for="hueModifier">Color Hue Modifier <small>Number 0-360</small></label>
-						<input type="number" class="form-control" data-key="hueModifier" id="hueModifier" placeholder="0" min="0" max="360">
-					</div>
-					<div class="form-group">
-						<label for="lumModifier">Color Brightness Modifier <small>Number 0-80, lower is brighter.</small></label>
-						<input type="number" class="form-control" data-key="lumModifier" id="lumModifier" placeholder="0" min="0" max="100">
-					</div>
-					<div class="form-group">
-						<button type="button" class="btn btn-success form-control" id="saveSettings">
-							<i class="fa fa-fw fa-save"></i> Save Tag Settings
-						</button>
-					</div>
+
+	<div class="panel panel-primary">
+		<div class="panel-heading"><span class="panel-title">Topic Titles</span></div>
+		<div class="panel-body">
+			<div class="form-group">
+				<div class="checkbox">
+					<label for="topicsEnabled">
+						<input type="checkbox" data-key="topicsEnabled" id="topicsEnabled" />
+						Enable colored topic titles.
+					</label>
+				</div>
+				<div class="checkbox">
+					<label for="topicsModsOnly">
+						<input type="checkbox" data-key="topicsModsOnly" id="topicsModsOnly" />
+						Only allow moderators to use colors in topic titles.
+					</label>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="panel panel-primary">
+		<div class="panel-heading"><span class="panel-title">Tags</span></div>
+		<div class="panel-body">
+			<div class="form-group">
+				<div class="checkbox">
+					<label for="tagsEnabled">
+						<input type="checkbox" data-key="tagsEnabled" id="tagsEnabled" />
+						Enable colored tags.
+					</label>
+				</div>
+				<div class="checkbox">
+					<label for="tagsGradient">
+						<input type="checkbox" data-key="tagsGradient" id="tagsGradient" />
+						Use gradients in tags.
+					</label>
+				</div>
+				<div>
+					<label for="hueModifier">Color Hue Modifier <small>Number 0-360</small></label>
+					<input type="number" class="form-control" data-key="hueModifier" id="hueModifier" placeholder="0" min="0" max="360">
+				</div>
+				<div>
+					<label for="lumModifier">Color Brightness Modifier <small>Number 0-80, lower is brighter.</small></label>
+					<input type="number" class="form-control" data-key="lumModifier" id="lumModifier" placeholder="0" min="0" max="100">
 				</div>
 			</div>
 		</div>
@@ -84,12 +142,9 @@
 </form>
 
 <script type="text/javascript">
-
 require(['settings'], function(settings) {
-
 	settings.sync('rainbows', $('#rainbows'));
-
-	$('#save, #saveSettings').click( function (event) {
+	$('.rainbows-save').click( function (event) {
 		settings.persist('rainbows', $('#rainbows'), function(){
 			socket.emit('admin.settings.syncRainbows');
 		});
